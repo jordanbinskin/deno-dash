0b187ab3def4a9509892fc312897dd4307f93a6f
jordanbinskin <jordan.binskin@gmail.com>
2021-10-28 23:18:07 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
-function handler(req) {
-  return new Response("Hello world");
+const App = () => {
+  return (
+    <html>
+      <body>
+       
+         <canvas id="myCanvas" width="480" height="320"></canvas>
+          <script src="script.js"></script>
+        <script>
+          alert('wassup')
+        </script>
+      </body>
+    </html>
+  )
+}
+
+function handler(req: Request): Response {
+  const renderedHTML = renderSSR(<App />)
+  return new Response(renderedHTML, {
+    headers: {
+      "content-type": "text/html"
+    }
+  });
 }
 
 console.log("Listening on http://localhost:8000");
